54d97cba522fa6213e6998fa53b8a297320cb10d
Mario Palamari <mariopalamari@gmail.com>
2025-02-11 20:52:20 +0100

    creación base de datos

---
+++ database/migrations/2025_02_11_193522_create_restaurante_red_social_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurante_red_social', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_restaurante')->constrained('restaurantes');
+            $table->foreignId('id_red_social')->constrained('redes_sociales');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurante_red_social');
+    }
+};
