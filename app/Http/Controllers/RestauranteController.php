9284939d0d10c97be197b090a9ecaefabb10e96c
Darckfer <juliocesarcarrillorocha@gmail.com>
2025-02-14 15:58:44 +0100

    filtros restaurantes con ajax correccion de el css

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -9,36 +9,58 @@ use Illuminate\Support\Facades\DB;
 
 class RestauranteController extends Controller
 {
-    public function restaurantes()
+    public function mostrarpagina()
     {
-        $restaurantes = Restaurante::select(
+        return view('restaurantes.restaurantes');
+    }
+
+    public function mostrarrestaurantes(Request $request)
+    {
+        $query = Restaurante::select([
             'restaurantes.id',
             'restaurantes.nombre',
             'restaurantes.descripcion',
             'restaurantes.precio_medio',
             'restaurantes.img',
-            DB::raw('COALESCE(ROUND(AVG(valoraciones.valoracion), 2), 0) AS media_valoracion')
-        )
+            DB::raw('COALESCE(ROUND(AVG(valoraciones.valoracion), 2), 0) AS media_valoracion'),
+            DB::raw('GROUP_CONCAT(DISTINCT etiquetas.nombre SEPARATOR ", ") AS etiquetas')
+        ])
             ->leftJoin('valoraciones', 'restaurantes.id', '=', 'valoraciones.id_restaurante')
-            ->groupBy('restaurantes.id', 'restaurantes.nombre')
-            ->orderByDesc('media_valoracion')
+            ->leftJoin('restaurante_etiqueta', 'restaurantes.id', '=', 'restaurante_etiqueta.id_restaurante')
+            ->leftJoin('etiquetas', 'restaurante_etiqueta.id_etiqueta', '=', 'etiquetas.id')
+            ->groupBy('restaurantes.id');
+
+        // FILTRO POR PRECIO
+        if ($request->precio) {
+            $precio = $request->precio;
+            $query->where('restaurantes.precio_medio', '>=', "$precio");
+        }
+
+        // FILTRO POR TIPO DE COCINA (ETIQUETAS)
+        if ($request->tipo_cocina) {
+            $query->whereExists(function ($subquery) use ($request) {
+                $subquery->select(DB::raw(1))
+                    ->from('restaurante_etiqueta')
+                    ->join('etiquetas', 'restaurante_etiqueta.id_etiqueta', '=', 'etiquetas.id')
+                    ->whereRaw('restaurantes.id = restaurante_etiqueta.id_restaurante')
+                    ->where('etiquetas.nombre', 'LIKE', '%' . $request->tipo_cocina . '%');
+            });
+        }
+
+        // FILTRO POR VALORACIÓN
+
+        if ($request->valoracion) {
+            // having es para condición de búsqueda para un grupo o agregado.
+            $query->having('media_valoracion', '>=', $request->valoracion);
+        }
+
+        // ORDENAR POR MEJOR VALORADOS
+        $restaurantes = $query->orderByDesc('media_valoracion')
             ->paginate(5);
 
-        // $restaurantes = Restaurante::select(
-        //     'restaurantes.*',
-        //     'e.*',
-        //     DB::raw('ROUND(AVG(v.valoracion), 2) AS media_valoracion')
-
-        // )
-        //     ->join('valoraciones as v', 'v.id_restaurante', '=', 'restaurantes.id')
-        //     ->join('restaurante_etiqueta as re', 're.id_restaurante', '=', 'restaurantes.id')
-        //     ->join('etiquetas as e', 'e.id', '=', 're.id_etiqueta')
-        //     ->groupBy('restaurantes.id')
-        //     ->get();
-
         // print_r($restaurantes);
-
-        return view('restaurantes.restaurantes', compact('restaurantes'));
+        return response()->json(['restaurantes' => $restaurantes]);
+        // return view('restaurantes.restaurantes', compact('restaurantes'));
     }
 
     public function inforestaurante($nombre, Request $request)
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestauranteController;
 
+// Route::get('/', function () {
+//     return view('restaurantes.restaurantes');
+// })->name('restaurantes');
+
 route::controller(RestauranteController::class)->group(function () {
-    route::get('/', 'restaurantes')->name('restaurantes.restaurantes');
+    Route::get('/restaurantes', 'mostrarpagina')->name('restaurantes.restaurantes');
+    Route::post('/mostrarrestaurantes', 'mostrarrestaurantes')->name('mostrarrestaurantes');
     route::post('/restaurantes/{nombre}', 'inforestaurante')->name('restaurante.ver');
 });
